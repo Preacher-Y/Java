7a29c6f83144e4b71cfdb0158d108a98a20c0939
Preacher-Y <ysheja@gmail.com>
2025-08-02 10:26:13 +0200

    Java: - Learning about string builders

--- Tutorial1Stringbuilder/StringBuilderClass.java
+++ Tutorial1Stringbuilder/StringBuilderClass.java
@@ -1,13 +1,14 @@
-package Tutorial1;
+package Tutorial1Stringbuilder;
 public class StringBuilderClass{
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
         // Using StringBuilder to build a string
+        StringBuilder sb = new StringBuilder();
         sb.append("Hello");
         sb.append(" World");
         // Convert StringBuilder to String
         String result = sb.toString();
-        System.out.println(result);
+        int age=32;
+        System.out.println(String.format("%s this world is for people with age of %d",result,age));
         
     }
 }
