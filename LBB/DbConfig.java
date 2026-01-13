03ec499f0e8104455eb90f65034cb4913ae00dbc
Preacher-Y <ysheja@gmail.com>
2026-01-13 21:14:51 +0200

    small tweaks

--- LBB/DbConfig.java
+++ LBB/DbConfig.java
@@ -2,10 +2,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DbConfig {
+    //DB Data
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
     private static final String URL = "jdbc:mysql://localhost:3306/lbb";
 
+    //Conection
     public static Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(URL,USERNAME,PASSWORD);
